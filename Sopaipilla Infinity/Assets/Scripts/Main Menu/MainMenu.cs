cf0c778e991db5eafd3c8be57fd3e011370b79d7
Wulfheim89 <j.wulfballesteros@uandresbello.edu>
2023-04-24 01:16:05 -0400

    build 0.1 presentación 24/4

---
+++ Sopaipilla Infinity/Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void EscenaJuego()
+    {
+        SceneManager.LoadScene("Nivel1Templo");
+    }
+
+    public void CargarNivel(string nombreNivel)
+    {
+        SceneManager.LoadScene(nombreNivel);
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+    
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Creditos()
+    {
+        SceneManager.LoadScene("Creditos");
+    }
+}
